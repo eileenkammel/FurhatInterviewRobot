65ceb5ce2d1f16bb6c84fa44e5c6f9e6e57cef59
Eileen Kammel <eileen.niedenfuehr@uni-potsdam.de>
2023-08-22 13:17:04 +0200

    writing audio might be working now

--- src/main/kotlin/furhatos/app/interviewrobot/main.kt
+++ src/main/kotlin/furhatos/app/interviewrobot/main.kt
@@ -1,7 +1,8 @@
 package furhatos.app.interviewrobot
 
+import furhatos.app.interviewrobot.audiofeed.FurhatAudioFeedPlayback
 import furhatos.app.interviewrobot.flow.Init
-import furhatos.demo.audiofeed.FurhatAudioFeedRecorder
+import furhatos.app.interviewrobot.audiofeed.FurhatAudioFeedRecorder
 import furhatos.demo.audiofeed.FurhatAudioFeedStreamer
 import furhatos.flow.kotlin.Flow
 import furhatos.skills.Skill
@@ -15,13 +16,25 @@ class InterviewrobotSkill : Skill() {
 
 fun main(args: Array<String>) {
     val streamer = FurhatAudioFeedStreamer()
+    streamer.start("192.168.0.9")
+    recordAudio(streamer)
+    /** Choose one of them to record audio in, out or both */
+    //recorder.startRecordAll(File("recording.wav"))
+    println("Starting streaming, press return to stop.")
+    //readlnOrNull()
+    Skill.main(args)
+    streamer.stop()
+}
+
+fun recordAudio(streamer: FurhatAudioFeedStreamer) {
     val recorder = FurhatAudioFeedRecorder(streamer)
 
     /** Choose one of them to record audio in, out or both */
-    //recorder.startRecordAll(File("recording.wav"))
+//    recorder.startRecordAll(File("recording.wav"))
     recorder.startRecordSeparate(audioInFile = File("audioIn.wav"), audioOutFile = File("audioOut.wav"))
-    Skill.main(args)
+}
 
-
-    //streamer.stop()
+fun playbackAudio(streamer: FurhatAudioFeedStreamer) {
+    val playback = FurhatAudioFeedPlayback(streamer)
+    playback.start(playSystem = true, playUser = true)
 }
