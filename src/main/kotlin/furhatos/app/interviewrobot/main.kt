56ac4328710db01461ff930336572f4362b66301
Eileen Kammel <eileen.niedenfuehr@uni-potsdam.de>
2023-08-22 16:46:49 +0200

    delete unneeded audio stuff

--- src/main/kotlin/furhatos/app/interviewrobot/main.kt
+++ src/main/kotlin/furhatos/app/interviewrobot/main.kt
@@ -1,9 +1,6 @@
 package furhatos.app.interviewrobot
 
-import furhatos.app.interviewrobot.audiofeed.FurhatAudioFeedPlayback
 import furhatos.app.interviewrobot.flow.Init
-import furhatos.app.interviewrobot.audiofeed.FurhatAudioFeedRecorder
-import furhatos.demo.audiofeed.FurhatAudioFeedStreamer
 import furhatos.flow.kotlin.Flow
 import furhatos.skills.Skill
 import java.io.File
@@ -15,26 +12,5 @@ class InterviewrobotSkill : Skill() {
 }
 
 fun main(args: Array<String>) {
-    val streamer = FurhatAudioFeedStreamer()
-    streamer.start("192.168.0.9")
-    recordAudio(streamer)
-    /** Choose one of them to record audio in, out or both */
-    //recorder.startRecordAll(File("recording.wav"))
-    println("Starting streaming, press return to stop.")
-    //readlnOrNull()
     Skill.main(args)
-    streamer.stop()
-}
-
-fun recordAudio(streamer: FurhatAudioFeedStreamer) {
-    val recorder = FurhatAudioFeedRecorder(streamer)
-
-    /** Choose one of them to record audio in, out or both */
-//    recorder.startRecordAll(File("recording.wav"))
-    recorder.startRecordSeparate(audioInFile = File("audioIn.wav"), audioOutFile = File("audioOut.wav"))
-}
-
-fun playbackAudio(streamer: FurhatAudioFeedStreamer) {
-    val playback = FurhatAudioFeedPlayback(streamer)
-    playback.start(playSystem = true, playUser = true)
 }
